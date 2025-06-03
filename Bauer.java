f263ad43c9453a83e230e063f7a208bb70e997a5
christophjohn <christophjohn877@gmail.com>
2025-06-03 09:51:46 +0100

    Klassen

--- Bauer.java
+++ Bauer.java
@@ -13,25 +13,36 @@ public class Bauer extends Figur
     {
         return feld;
     }
-    @Override public ArrayList möglicheZüge()
-    {
-        ArrayList<Feld> M = new ArrayList();
-        feld.brettGeben().felder.indexOf(feld) + 1 = int i
-            if(feld.brettGeben().felder(i).figurGeben() == null)
-            {
-                M.add(feld.brettGeben().felder(i));
-            }
-            if(ersterZug == true)
-            {
-                if(feld.brettGeben().felder(i+1).figurGeben() == null && if(feld.brettGeben().felder(i).figurGeben() == null))
-                {
-                    M.add(feld.brettGeben().felder(i+1));
-                }
-            }
-            if(feld.brettGeben().schwarz.contains(this))
-            {
-            if(feld.brettGeben().schwarz.contains(feld.brettGeben().felder(i+8).figurGeben()) == true)
-            }
-    }
+    @Override
+    public ArrayList<Feld> möglicheZüge() {
+        ArrayList<Feld> M = new ArrayList<>();
+        ArrayList<Feld> felder = feld.brettGeben().felder;
+        int i = felder.indexOf(feld) + 1;
 
+        // Prüfe, ob i im gültigen Bereich ist
+        if (i < felder.size() && felder.get(i).figurGeben() == null) {
+            M.add(felder.get(i));
+        }
+
+        if (ersterZug) {
+            // Prüfe, ob i+1 im gültigen Bereich ist
+            if (i + 1 < felder.size() && felder.get(i).figurGeben() == null && felder.get(i + 1).figurGeben() == null)
+            {
+                M.add(felder.get(i + 1));
+            }
+        }
+
+        if (feld.brettGeben().schwarz.contains(this)) {
+            // Prüfe, ob i+8 und i-8 im gültigen Bereich sind
+            if (i + 8 < felder.size() && feld.brettGeben().weiß.contains(felder.get(i + 8).figurGeben()))
+            {
+                M.add(felder.get(i + 8));
+            }
+            if (i - 8 >= 0 && feld.brettGeben().weiß.contains(felder.get(i - 8).figurGeben()))
+            {
+                M.add(felder.get(i - 8));
+            }
+        }
+        return M;
+    }
 }
