09cb3af5414080cd03193d5db2a1fb8d634ac350
christophjohn <christophjohn877@gmail.com>
2025-06-04 09:21:16 +0100

    Klassen

--- König.java
+++ König.java
@@ -1,53 +1,47 @@
 import java.util.ArrayList;
 
-public class König extends Figur
-{
+public class König extends Figur {
     private Feld feld;
-    public König(Feld feld)
-    {
+
+    public König(Feld feld) {
         this.feld = feld;
     }
-    public Feld getFeld()
-    {
+
+    public Feld getFeld() {
         return feld;
     }
+
     @Override
-    public ArrayList<Feld> möglicheZüge()
-    {
+    public ArrayList<Feld> möglicheZüge() {
         ArrayList<Feld> M = new ArrayList<>();
         ArrayList<Feld> felder = feld.brettGeben().felder;
         int index = felder.indexOf(feld);
 
         int[] directions = {-9, -8, -7, -1, 1, 7, 8, 9};
 
-        if (feld.brettGeben().schwarz.contains(this))
-        {
-            for (int dir : directions)
-            {
+        if (feld.brettGeben().schwarz.contains(this)) {
+            for (int dir : directions) {
                 int newIndex = index + dir;
 
-                if (newIndex >= 0 && newIndex < 64)
-                {
+                if (newIndex >= 0 && newIndex < 64) {
                     if (Math.abs((newIndex % 8) - (index % 8)) > 1) continue;
 
                     Feld zielFeld = felder.get(newIndex);
                     Figur figur = zielFeld.figurGeben();
 
-                    if (figur == null || feld.brettGeben().weiß.contains(figur))
-                    {
+                    if (figur == null || feld.brettGeben().weiß.contains(figur)) {
                         M.add(zielFeld);
                     }
                 }
             }
+        }
+        return M;
+    }
 
-
-            public boolean obSetztSchach()
-        {
+    public boolean obSetztSchach() {
         ArrayList<Feld> list = this.möglicheZüge();
-        for (int i = 0; i < list.size(); i++)
-        {
-            if (list.get(i).figurGeben() instanceof König)
-            {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).figurGeben() instanceof König) {
                 return true;
             }
         }
